eeb3db97d79114955faa2d306e2511fa3e3e99f9
Andy Klimczak <andyklimczak@fastmail.com>
2017-06-05 22:33:55 -0400

    initial infinite scroll

--- src/components/Twitch.vue
+++ src/components/Twitch.vue
@@ -1,37 +1,69 @@
 <template>
   <div class="twitch">
     <stream v-for="stream in streams" :key="stream.id" :stream="stream"></stream>
+    <infinite-loading :on-infinite="onInfinite" ref="infiniteLoading"></infinite-loading>
   </div>
 </template>
 
 <script>
 import Twitch from 'twitch.tv-api'
+import InfiniteLoading from 'vue-infinite-loading'
 import Stream from './Stream'
+
 export default {
   name: 'twitch',
   components: {
-    Stream
+    Stream,
+    InfiniteLoading
   },
   data () {
     return {
-      streams: []
+      offset: 0,
+      limit: 8,
+      streams: [],
+      twitch: null,
+      loading: false
     }
   },
   created () {
     console.log('created')
-    const twitch = new Twitch({
+    this.twitch = new Twitch({
       id: '462kn4pnv92z2eef9e7kjphltdz5hy'
     })
-    return twitch.getTopStreams()
-        .then(data => {
-          console.log(data)
-          return data.streams
-        }).then(streams => {
-          console.log(streams)
-          this.streams = streams
-        }).catch(error => {
-          console.error(error)
-        })
+    this.fetchStreams()
+  },
+  updated () {
+    console.log('update')
+    this.loading = false
+    setTimeout(() => {
+      this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded')
+    }, 3000)
+  },
+  methods: {
+    onInfinite () {
+      console.log('on inf')
+      this.fetchStreams()
+    },
+    fetchStreams () {
+      if (!this.loading) {
+        this.loading = true
+        const options = {
+          offset: this.offset,
+          limit: this.limit
+        }
+        return this.twitch.getTopStreams(options)
+          .then(data => {
+            console.log(data)
+            return data.streams
+          }).then(streams => {
+            console.log(streams)
+            this.streams = this.streams.concat(streams)
+            this.offset += 4
+          }).catch(error => {
+            console.error(error)
+          })
+      }
+    }
   }
 }
 </script>
