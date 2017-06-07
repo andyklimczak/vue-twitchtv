b6ab88ae6204cf256c129ae6474e6d52b7524501
Andy Klimczak <andyklimczak@fastmail.com>
2017-06-06 22:30:23 -0400

    start nav

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
+    <home-menu></home-menu>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import HomeMenu from '@/components/HomeMenu'
+  export default {
+    name: 'app',
+    components: {
+      HomeMenu
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 30px;
+  }
 </style>
---
+++ src/components/HomeMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+      <div class="pure-u-1-5">
+        <a class="pure-menu-heading" href="">Your Site</a>
+      </div>
+
+      <div class="pure-u-4-5">
+        <ul class="pure-menu-list">
+          <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Home</a></li>
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Tour</a></li>
+          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Sign Up</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home-menu',
+    data () {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
